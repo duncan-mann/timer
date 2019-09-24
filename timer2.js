9ec243d12e6cc5ed9d0f05eb5a5b87ce493fd8cc
Duncan Mann <duncan.s.mann@gmail.com>
2019-09-24 19:47:45 +0000

    Timer2 function with closing bug to fix

--- timer2.js
+++ timer2.js
@@ -3,28 +3,34 @@ const readline = require('readline');
 const ask = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  SIGINT: 'Thanks for using me, ciao!'
 });
+
+// const close = function() {
+//   console.log('Thanks for using me, caio!');
+//   ask.close();
+// }
 
 const beep = function() {
   ask.question('What do you want to do?', (answer) => {
     if (answer === 'b') {
       process.stdout.write('\x07');
-      answer = beep();
+      return beep();
     } else if (answer >= 1 && answer <= 9) {
       console.log(`setting timer for ${answer} seconds...`);
       setTimeout(() => {
         process.stdout.write('\x07');
       }, answer * 1000);
-      answer = beep();
-    }
-    ask.on('SIGINT', () => {
-        ask.close();
-    });
+      return beep();
+  };
+  ask.on('SIGINT', () => {
+    console.log('\nThanks for using me, ciao!');
+    ask.close();
   });
+});
 };
-
 beep();
+
+
 
 
 // let args = process.argv.slice(2);
