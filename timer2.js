250e521bec199268a69bde063bf0177a033cce23
Duncan Mann <duncan.s.mann@gmail.com>
2019-09-24 20:03:21 +0000

    Functioning timer2 assignment

--- timer2.js
+++ timer2.js
@@ -14,35 +14,24 @@ const beep = function() {
   ask.question('What do you want to do?', (answer) => {
     if (answer === 'b') {
       process.stdout.write('\x07');
-      return beep();
+      answer = beep();
     } else if (answer >= 1 && answer <= 9) {
       console.log(`setting timer for ${answer} seconds...`);
       setTimeout(() => {
         process.stdout.write('\x07');
       }, answer * 1000);
-      return beep();
+      answer = beep();
   };
+}); 
+};
+
+const timer = function() {
+  beep();
   ask.on('SIGINT', () => {
     console.log('\nThanks for using me, ciao!');
     ask.close();
   });
-});
-};
-beep();
+}
 
+timer();
 
-
-
-// let args = process.argv.slice(2);
-
-// const beep = function(array) {
-//   for(let each of array) {
-//     if(each > 0) {
-//       setTimeout( () => {
-//         process.stdout.write('\x07');
-//       }, each * 1000);
-//     }
-//   }
-// };
-
-// beep(args);
