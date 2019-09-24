eb261e508b695178989667442bfd42d056e0dbf3
Tammie Chung <tammie.chung11@gmail.com>
2019-09-24 17:50:56 +0000

    debugged timer function

--- timer1.js
+++ timer1.js
@@ -2,10 +2,12 @@ let args = process.argv.slice(2);
 
 const beep = function(array) {
   for(let each of array) {
-    setTimeout( () => {
-      process.stdout.write('\x07');
-    }, each * 1000);
+    if(each > 0) {
+      setTimeout( () => {
+        process.stdout.write('\x07');
+      }, each * 1000);
+    }
   }
-}
+};
 
 beep(args);
